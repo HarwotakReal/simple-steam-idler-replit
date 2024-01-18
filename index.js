588083f7bade6a042ed972b31239cf97f874f052
HarwotakReal <103434080+HarwotakReal@users.noreply.github.com>
2024-01-18 20:28:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ user.on('loggedOn', () => {
 
 var username2 = process.env.username2;
 var password2 = process.env.password2;
-var shared_secret2 = process.env.shared2;
+var shared_secret2 = "";
 
 var games2 = [730, 700330];  // Enter here AppIDs of the needed games
 var status2 = 1;  // 1 - online, 7 - invisible
